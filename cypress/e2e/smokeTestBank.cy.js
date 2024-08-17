d66faeec781b82e354af9dcc4cb75080e9e76d6c
Gabriel Azubel <gabrielazubel63t@gmail.com>
2024-08-17 00:07:17 -0300

    Add first E2E test

--- cypress/e2e/smokeTestBank.cy.js
+++ cypress/e2e/smokeTestBank.cy.js
@@ -15,13 +15,32 @@ describe("conjunto de pruebas", () => {
 describe("Test suite - conjunto de pruebas", () => {
     it("Validar página de inicio", () => {
         // prefijo "cy." + método
-        cy.visit("https://zero.webappsecurity.com/")
+        cy.visit("http://zero.webappsecurity.com/")
 
         //Ubico el elemento y luego valido que esté visible y contenga cierto texto
         /*
             cy.get("h1").should("be.visible")
             cy.get("h1").contains("It works!")
         */
-        cy.get("h1").should("be.visible").contains("It works!")
+        cy.get('.brand').should("be.visible").contains("Zero Bank")
+    })
+
+    it("Prueba E2E - Transferencia de fondos", () => {
+        cy.visit("http://zero.webappsecurity.com/")
+        cy.get('#signin_button').click()
+        cy.get('#user_login').type("username") //Para escribir dentro de un input
+        cy.get('#user_password').type("password")
+        cy.get('.btn').click()
+        cy.get('#transfer_funds_tab > a').click()
+        cy.get('#tf_fromAccountId').select("1") //Seleccionar desde un select mediante su posición - Empieza en 1    
+        cy.get('#tf_toAccountId').select("5")
+        cy.get('#tf_amount').type("300")
+        cy.get('#tf_description').type("Transferencia de prueba de 300")
+        cy.get('#btn_submit').click()
+        cy.get('#btn_submit').click()
+
+        //Validación final
+
+        cy.get('.alert').contains("You successfully submitted your transaction.")
     })
 })
